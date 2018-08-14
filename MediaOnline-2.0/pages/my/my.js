34b8b0f6584d57786dd1cc27f958237e1a81f6ba
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-08-14 14:40:08 +0800

    add my page ui

--- MediaOnline-2.0/pages/my/my.js
+++ MediaOnline-2.0/pages/my/my.js
@@ -5,7 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    functions1: [
+      {
+        "id": 1,
+        "name": "已购清单",
+        "pic_url": '../../images/my_purchase.png'
+      },
+      {
+        "id": 2,
+        "name": "播放记录",
+        "pic_url": '../../images/my_record.png'
+      },
+      {
+        "id": 3,
+        "name": "我喜欢的",
+        "pic_url": '../../images/my_like.png'
+      },
+      {
+        "id": 4,
+        "name": "我的拼团",
+        "pic_url": '../../images/my_puzzle.png'
+      },
+      {
+        "id": 5,
+        "name": "回应我的",
+        "pic_url": '../../images/my_reply.png'
+      },
+      {
+        "id": 6,
+        "name": "我的集赞",
+        "pic_url": '../../images/my_praise.png'
+      },
+      {
+        "id": 7,
+        "name": "书币钱包",
+        "pic_url": '../../images/my_wallet.png'
+      },
+      {
+        "id": 8,
+        "name": "系统消息",
+        "pic_url": '../../images/my_sys_message.png'
+      },
+      {
+        "id": 9,
+        "name": "帮助中心",
+        "pic_url": '../../images/my_help.png'
+      }
+    ],
+    functions2: [
+      {
+        "id": 12,
+        "name": "回复我的",
+        "pic_url": '../../images/ic_message.png'
+      },
+      {
+        "id": 13,
+        "name": "收到的赞",
+        "pic_url": '../../images/ic_message.png'
+      },
+      {
+        "id": 14,
+        "name": "系统通知",
+        "pic_url": '../../images/ic_message.png'
+      }
+    ]
   },
 
   /**
