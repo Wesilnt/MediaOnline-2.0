b3b8fd67c9bdebb3ca31d556c1bb7f314f006f08
MarkWangLike <wh197126@163.com>
2018-08-14 18:19:42 +0800

    首页UI大致完成

--- MediaOnline-2.0/pages/home/home.js
+++ MediaOnline-2.0/pages/home/home.js
@@ -5,7 +5,102 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    knowledge:[{
+      title:"文学|为什么抱元科技没有食堂？",
+      id:""
+    },
+    {
+      title: "艺术|名画为什么这么值钱？",
+      id: ""
+    }],
+    vision:[{
+      cover:"",
+      title:"中华五千年（上）",
+      des:"听历史故事，涨人生见识",
+      author:"伍智·国学教师",
+      price:"99.00",
+      count:"100",
+      fit:"4-6岁",
+    },
+    {
+      cover: "",
+      title: "中华五千年（上）",
+      des: "听历史故事，涨人生见识",
+      author: "伍智·国学教师",
+      price: "99.00",
+      count: "100",
+      fit: "4-6岁",
+    },
+    {
+      cover: "",
+      title: "中华五千年（上）",
+      des: "听历史故事，涨人生见识",
+      author: "伍智·国学教师",
+      price: "99.00",
+      count: "100",
+      fit: "4-6岁儿童",
+    }],
+    books:[
+    {
+      cover:"",
+      title:"小麦的故事",
+      price:"4.99",
+      isNew:true
+    },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: true
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: true
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: true
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: true
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: false
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: false
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: false
+      },
+      {
+        cover: "",
+        title: "小麦的故事",
+        price: "4.99",
+        isNew: false
+      },
+    ]
   },
 
   /**
@@ -13,6 +108,11 @@ Page({
    */
   onLoad: function (options) {
   
+  },
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
   },
 
   /**
